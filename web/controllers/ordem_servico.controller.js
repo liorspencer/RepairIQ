f01f46859498d01eea9fac85679cafdfdad328d4
liorspencer <juliodiversity@gmail.com>
2025-05-26 17:01:17 -0300

    criado 3 models novos, atualizar ordem de serviço controller

---
+++ web/controllers/ordem_servico.controller.js
@@ -0,0 +1,23 @@
+
+const mostrarOrdemServico = async (req, res) => {
+
+};
+
+const cadastrarOrdemServico = async (req, res) => {
+
+};
+
+const editarOrdemServico = async (req, res) => {
+
+};
+
+const deletarOrdemServico = async (req, res) => {
+
+};
+
+module.exports = {
+    mostrarOrdemServico,
+    cadastrarOrdemServico,
+    editarOrdemServico,
+    deletarOrdemServico
+};
