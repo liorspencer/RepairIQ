9175cc8d75d4f238a0b3fdf2e11ba5455fc050f9
douglassvdc <doouglas_vdc@hotmail.com>
2025-05-26 20:07:28 -0300

    sistema de login e sharedPreference

--- android/app/src/main/java/com/worksync/repairiq/view/LoginActivity.java
+++ android/app/src/main/java/com/worksync/repairiq/view/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.worksync.repairiq.view;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,23 +17,59 @@ import androidx.core.view.WindowInsetsCompat;
 import com.worksync.repairiq.R;
 
 public class LoginActivity extends AppCompatActivity {
+
     private Button bEntrar;
+
+    private EditText editUsuario, editSenha;
+    private CheckBox checkBoxLogado;
+    private SharedPreferences preferences;
+    private static final String PREF_NAME = "LoginPrefs";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        bEntrar = findViewById(R.id.b_entrar);
-        bEntrar.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MenuPrincipalActivity.class);
-            startActivity(intent);
+
+        preferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        boolean isLogged = preferences.getBoolean("logado", false);
+
+        if (isLogged) {
+            startActivity(new Intent(this, MenuPrincipalActivity.class));
             finish();
+        }
+
+        editUsuario = findViewById(R.id.editTextUsuario);
+        editSenha = findViewById(R.id.editTextTextPassword);
+        checkBoxLogado = findViewById(R.id.checkBoxLogado);
+        bEntrar = findViewById(R.id.b_entrar);
+
+        bEntrar.setOnClickListener(v -> {
+            String usuario = editUsuario.getText().toString().trim();
+            String senha = editSenha.getText().toString().trim();
+
+            if (usuario.equals("admin") && senha.equals("1234")) {
+                if (checkBoxLogado.isChecked()) {
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean("logado", true);
+                    editor.apply();
+                }
+
+                Intent intent = new Intent(this, MenuPrincipalActivity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show();
+            }
+
         });
     }
+
 }
