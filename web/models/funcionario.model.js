66b9f6dcedde66f3f43160609b5cd9647c32778e
liorspencer <juliodiversity@gmail.com>
2025-05-26 21:45:51 -0300

    feitas alterações no BD e em modelos.

--- web/models/funcionario.model.js
+++ web/models/funcionario.model.js
@@ -1,3 +1,23 @@
-async function name(params) {
+async function cadastrarFuncionario(pool, funcionario) {
+    const sql = `INSERT INTO funcionario (nome, login, senha) VALUES (?, ?, ?)`;
+    const valores = [funcionario.nome, funcionario.login, funcionario.senha];
+    const [resultado] = await pool.execute(sql, valores);
+    return resultado;
+}
+
+async function buscarFuncionarios(pool) {
     
+}
+
+async function buscarLogin(pool, login) {
+    const sql = `SELECT * FROM funcionario WHERE login = ?`;
+    const [resultado] = await pool.execute(sql, login);
+    return resultado;
+}
+
+async function Funcionario(pool, id, estado) {
+    const sql = `UPDATE funcionario SET ativo = ? WHERE id - ?`;
+    const valores = [estado, id];
+    const [resultado] = await pool.execute(sql, valores);
+    return resultado;
 }
